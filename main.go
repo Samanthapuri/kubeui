240782cc352ee15998dba7f42a4b81640c6a7fd2
Bindhupriya <priya@BindhupriyasAir.attlocal.net>
2024-07-27 00:21:40 -0500

    added new line

--- main.go
+++ main.go
@@ -8,5 +8,9 @@ import "github.com/Samanthapuri/kubeui/cmd"
 
 func main() {
 	cmd.Execute()
+<<<<<<< Updated upstream
 	fmt.Println("this is test line")
+=======
+        fmt.Println("this is local test line")
+>>>>>>> Stashed changes
 }
